5bd640160165701af45b298b0510acb6388a4bf5
chikamasuda <c.m.062820@gmail.com>
2022-07-05 22:14:36 +0900

    ショップ詳細ページを修正

--- pages/shop/_id.vue
+++ pages/shop/_id.vue
@@ -9,7 +9,20 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      shopList: '',
+    };
+  },
+  async getShopList() {
+    await this.$axios.get('/api/v1/shops')
+    .then((response) => {
+      this.shopList = response.data.shops;
+    })
+    .catch((error) => {
+      console.log(error.response);
+    })
+  },
 }
 </script>
 
