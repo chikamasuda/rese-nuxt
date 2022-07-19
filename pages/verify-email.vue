687cc82ef0ad4f6bca60890f3f5913eb90905499
chikamasuda <c.m.062820@gmail.com>
2022-07-20 01:18:00 +0900

    メール認証終了画面の文言訂正

--- pages/verify-email.vue
+++ pages/verify-email.vue
@@ -5,7 +5,7 @@
     <LoginMenu />
   </div>
   <v-card class="mx-auto message-card bg-blue text-left">
-    <p class="thanks-text">メール認証が行われました。<br>ログイン画面からログインをお願いします。</p>
+    <p class="thanks-text">メール認証が完了しました。<br>ログイン画面からログインをお願いします。</p>
     <div class="text-center">
       <v-btn small class="blue accent-4 white--text mt-5" type="submit" to="/login">ログインする</v-btn>
     </div>
